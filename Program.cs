05b5048f53bfe9b11fbf54c4d73d222e95e25b5e
PetterC <petterchan89@gmail.com>
2016-11-14 08:13:24 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                User user = new User();
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+
+                tcpclnt.Connect("10.2.20.50", 8000);
+            
+                Console.WriteLine("Connected");
+
+                user.GetUserName();
+
+
+                Console.WriteLine();
+
+
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+
+                stm.Write(ba, 0, ba.Length);
+
+
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+                Console.ReadLine();
+
+                tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+                Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}
+    
---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class User
+    {
+       
+        private string userName;
+
+        public void GetUserName()
+        
+        {
+            Console.WriteLine();
+           
+            Console.WriteLine("UserName: {0}", Environment.UserName);
+        }
+    }
+    
+    }
+
+
